98bca324a78652b2a1e719bef0e9d9972bf74518
Jvbass <juan.vpinoc@gmail.com>
2024-05-03 11:28:22 -0400

    ejercicio Perfil en Internet

--- Main.kt
+++ Main.kt
@@ -3,8 +3,10 @@ const val separador = "---------------------------------"
 fun main() {
     println("\n $separador Notificaciones Móviles $separador")
     notificacion()
+
     println("\n $separador PRECIO DE LA ENTRADA DE CINE $separador")
     precioEntradaCine()
+
     println("\n $separador Conversor de temperatura $separador")
     conversorTemperaturas()
 
@@ -12,6 +14,9 @@ fun main() {
     val mySong = Song("Bohemian Rhapsody", "Queen", 1975, 1500)
     mySong.imprimeDescripcion()
     println("¿Es popular? ${if (mySong.esPopular) "Sí" else "No"}")
+
+    println("\n $separador Perfil de Internet $separador")
+    perfilInternet()
 }
 
 /* 1. ---------------------    Notificaciones Móviles   --------------------------------------------
@@ -130,7 +135,7 @@ fun conversorTemperaturas() {
 
 
 /**/
-    printFinalTemperature(27.0,"Celsius","Fahrenheit", { Celsius -> Celsius * 9/5 + 32 }) //
+    printFinalTemperature(27.0,"Celsius","Fahrenheit") { Celsius -> Celsius * 9 / 5 + 32 } //
     /* printFinalTemperature(350.0,"Kelvin", "Celsius", { kelvin -> kelvin - 273.15 })
     printFinalTemperature(10.0,"Fahrenheit", "Kelvin", { fahrenheit -> (5/9) * (fahrenheit - 32) + 273.15 }*/
 }
@@ -169,5 +174,38 @@ class Song (
 
     fun imprimeDescripcion(){
         println("${this.titulo}, interpretada por ${this.artista}, se lanzo en ${this.anhoPublicacion}")
+    }
+}
+
+
+/*A menudo, debes completar los perfiles de sitios web en línea que contienen campos obligatorios y no obligatorios.
+Por ejemplo, puedes agregar tu información personal y un vínculo a otras personas que te refirieron para que registraras tu perfil.
+
+En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa que imprima los detalles
+del perfil de una persona.
+
+Name: Amanda
+Age: 33
+Likes to play tennis. Doesn't have a referrer.
+
+Name: Atiqah
+Age: 28
+Likes to climb. Has a referrer named Amanda, who likes to play tennis.
+*/
+
+fun  perfilInternet() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        println("\n Name: $name \n Age: $age")
+        if (hobby != null) println("Likes to $hobby.")
+        if (referrer != null) println("Has a referrer named ${referrer.name}, who likes to ${referrer.hobby}.")
+        else println("Doesn't have a referrer.")
     }
 }
